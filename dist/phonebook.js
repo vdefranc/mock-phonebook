f9a13e5e6ad5a222980d781d1ad38b10d94080ab
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-03-25 12:29:40 -0400

    edit toggling works

--- dist/phonebook.js
+++ dist/phonebook.js
@@ -140,22 +140,25 @@ var ContactView = Backbone.View.extend({
             <div class="col-sm-12">\
                 <h3>New Contact</h3>\
                  <div class="contact-view-button-wrapper">\
-                    <button class="btn btn-default" type="button">\
-                        <span class="glyphicon glyphicon-pencil"></span>\
+                    <button class="btn btn-default edit" type="button">\
+                        <span class="glyphicon glyphicon-edit"></span>\
                     </button>\
-                    <button class="btn btn-default" type="button">\
+                    <button class="btn btn-default delete" type="button">\
                         <span class="glyphicon glyphicon-trash"></span>\
                     </button>\
                 </div>\
                 <div class="contact-fields">\
                     <form>\
-                    <p>First Name:<input type="text" value="<%= first %>" readonly></p>\
-                    <p>Last Name:<input type="text" value="<%= last %>"" readonly></p>\
-                    <p>Phone Number:<input type="text" value="<%= phone %>"" readonly></p>\
+                    <p>First Name:<input name="first" type="text" value="<%= first %>" readonly></p>\
+                    <p>Last Name:<input name="last" type="text" value="<%= last %>"" readonly></p>\
+                    <p>Phone Number:<input name="phone" type="text" value="<%= phone %>"" readonly></p>\
                 </form>\
                 </div>\
             </div>\
 	'),
+	events: {
+		'click .edit': 'edit',
+	},
 	initialize: function () {
 		this.render();
 	},
@@ -174,8 +177,25 @@ var ContactView = Backbone.View.extend({
 
 		this.$el.html(questionHtml);
 		this.$container.append(this.$el);
-	}
+	},
+	edit: function () {
+		var editButton = $('.edit').find('span');
 
+		if (!editing) {
+			editing = true;
+			this.$el.find('input').attr("readonly", false)
+				.addClass('active-edit');
+
+			
+			editButton.removeClass('glyphicon-edit').addClass('glyphicon-floppy-save');
+		} else {
+			this.$el.find('input').attr("readonly", true)
+			.removeClass('active-edit');
+
+			editButton.removeClass('glyphicon-floppy-save').addClass('glyphicon-edit');
+		}
+		
+	}
 });
 
 var currentCollection = 0;
